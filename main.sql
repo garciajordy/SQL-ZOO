29e25ed38e30d07689f76d3ba4b560520a163648
garciajordy <jordygarcia0119@gmail.com>
2021-03-31 19:16:57 +0300

    Add select from world tutorial solutions

--- main.sql
+++ main.sql
@@ -42,3 +42,29 @@ ORDER BY name
 10) SELECT name FROM world
  WHERE name LIKE '____'
 
+-- SELECT from WORLD Tutorial
+
+1) SELECT name, continent, population FROM world
+
+2) SELECT name
+  FROM world
+ WHERE population > 200000000
+
+3) SELECT name, gdp/population FROM world WHERE population > 200000000
+
+4) SELECT name, population/1000000 FROM world WHERE continent = 'South America'
+
+5) SELECT name, population FROM world WHERE name IN ('France','Germany','Italy')
+
+6) SELECT name FROM world WHERE name LIKE '%United%'
+
+7) SELECT name, population, area FROM world WHERE area > 3000000 OR population > 250000000
+
+8)   SELECT name, population, area
+  FROM world
+  WHERE ((population > 250000000) OR (area > 3000000)) 
+       AND NOT ((population > 250000000) AND (area > 3000000))
+
+9) SELECT name, ROUND(population/1000000,2), ROUND(gdp/1000000000,2) FROM world WHERE continent = 'South America'
+
+10) 
