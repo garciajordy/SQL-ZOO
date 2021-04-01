2cb1cb97da4dbcd312dfb08a172320389713c1b5
garciajordy <jordygarcia0119@gmail.com>
2021-04-01 12:19:45 +0300

    Add solution for SUM and COUNT

--- main.sql
+++ main.sql
@@ -152,3 +152,23 @@ FROM world WHERE LEFT(name,1) = LEFT(capital,1) AND NOT name = capital
 8) SELECT continent, MIN(name) AS name
 FROM world 
 GROUP BY continent
+
+-- SUM and COUNT
+
+1) SELECT SUM(population)
+FROM world
+
+2) SELECT continent
+FROM world GROUP BY continent
+
+3) SELECT SUM(gdp) FROM world WHERE continent = 'Africa'
+
+4) SELECT count(name) FROM world WHERE area > 1000000
+
+5) SELECT SUM(population) FROM world WHERE name IN ('Estonia', 'Latvia', 'Lithuania')
+
+6) SELECT continent, count(name) FROM world GROUP BY continent
+
+7) SELECT continent, count(name) FROM world WHERE population > 10000000 GROUP BY continent
+
+8) SELECT continent FROM world GROUP BY continent HAVING SUM(population) >= 100000000;
