5949a00ca915f4ce4f13aebe09a88692901ec63c
garciajordy <jordygarcia0119@gmail.com>
2021-04-01 16:11:40 +0300

    Add solution for NSS Tutorial

--- main.sql
+++ main.sql
@@ -266,3 +266,42 @@ ELSE 'None'
 END
  FROM teacher
 
+-- NSS Tutorial
+
+1) SELECT A_STRONGLY_AGREE
+  FROM nss
+ WHERE question='Q01'
+   AND institution='Edinburgh Napier University'
+   AND subject='(8) Computer Science'
+
+2) SELECT institution, subject
+  FROM nss
+ WHERE question='Q15'
+   AND score = '100'
+
+3) SELECT institution,score
+  FROM nss
+ WHERE question='Q15'
+   AND score < 50
+   AND subject='(8) Computer Science'
+
+4) SELECT subject, SUM(response)
+  FROM nss
+ WHERE question='Q22'
+   AND subject='(8) Computer Science'
+ OR  subject='(H) Creative Arts and Design' AND question='Q22' GROUP BY subject
+
+5) SELECT subject, SUM(response*A_STRONGLY_AGREE/100) FROM nss
+	WHERE question='Q22' AND
+		(subject='(8) Computer Science' OR subject='(H) Creative Arts and Design')
+			GROUP BY subject
+
+6) SELECT subject, ROUND(SUM(response*A_STRONGLY_AGREE)/SUM(response),0) FROM nss
+WHERE question='Q22' AND subject IN ('(8) Computer Science','(H) Creative Arts and Design')
+GROUP BY subject
+
+7) SELECT institution, ROUND(SUM(response*score)/SUM(response),0) FROM nss
+WHERE question='Q22' AND (institution LIKE '%Manchester%') GROUP BY institution
+
+8) SELECT institution, SUM(sample), SUM(CASE WHEN subject LIKE '(8)%' THEN sample END) FROM nss
+WHERE question='Q01' AND (institution LIKE '%Manchester%') GROUP BY institution
